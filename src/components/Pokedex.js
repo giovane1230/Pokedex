2b2501a824ebe3530fe9ac42deec6ff003322237
giovane1230 <giovanef.96@gmail.com>
2025-05-04 20:17:13 -0300

    completo

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -1,30 +1,56 @@
 import React from 'react';
-import { arrayOf } from 'prop-types';
-
 import Pokemon from './Pokemon';
-import { pokemonType } from '../types';
+import pokemonList from '../data';
 
 class Pokedex extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      contador: 0,
+      filteredType: 'All',
+    };
+  }
+
+  incrementar = () => {
+    const filteredList = this.getFilteredList();
+    this.setState((prevState) => ({
+      contador: (prevState.contador + 1) % filteredList.length,
+    }));
+  };
+
+  setFilteredType = (type) => {
+    this.setState({ filteredType: type, contador: 0 });
+  };
+
+  getFilteredList = () => {
+    const { filteredType } = this.state;
+    return filteredType === 'All'
+      ? pokemonList
+      : pokemonList.filter((poke) => poke.type === filteredType);
+  };
+
   render() {
-    const { pokemonList } = this.props;
+    const { contador } = this.state;
+    const filteredList = this.getFilteredList();
+
     return (
       <>
-        <h1> Pokédex </h1>
+        <h1>Pokédex</h1>
         <div className="pokedex">
-          { pokemonList
-            .map((pokemon) => <Pokemon key={ pokemon.id } pokemon={ pokemon } />) }
+          {filteredList[contador] && (
+            <Pokemon
+              key={ filteredList[contador].id }
+              pokemon={ filteredList[contador] }
+            />
+          )}
+          <button onClick={ this.incrementar }>Próximo pokémon</button>
+          <button onClick={ () => this.setFilteredType('Fire') }>Fire</button>
+          <button onClick={ () => this.setFilteredType('Psychic') }>Psychic</button>
+          <button onClick={ () => this.setFilteredType('All') }>All</button>
         </div>
       </>
     );
   }
 }
-
-Pokedex.defaultProps = {
-  pokemonList: [],
-};
-
-Pokedex.propTypes = {
-  pokemonList: arrayOf(pokemonType),
-};
 
 export default Pokedex;
